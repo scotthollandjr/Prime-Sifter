807939c8eb02ad8fe257bd0fb65c09fa08626245
Scott Holland <scotthollandjr@gmail.com>
2016-02-01 17:00:20 -0800

    First failed attempt

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-scripts.js
+var primeList = [];
+var number = 100;
+
+for ( prime = 2; prime <= number; prime ++ ) {
+  primeList.push(prime);
+};
+
+var sifter = function() {
+for ( i = 2; i < 100; i ++ ) {
+  if ( primeList[i] % i = 0 ) {
+    primeList.pop(i);
+  }
+}
+};
+
+primeList.forEach(sifter);
